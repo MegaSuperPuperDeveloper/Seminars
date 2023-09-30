1122ef964ae599a34bb253872573ba2ff6047d8f
Your Name <you@example.com>
2023-09-30 11:48:57 +0300

    V1

---
+++ Task30/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = new int[8];
+array[0] = 2173;
+array[4] = 545;
+array[7] = 64;
+
+for(int i = 0; i < array.Length; i++) {
+    array[i] = i * i;
+    Console.Write($"{array[i]} ");
+}
