75609700a44e4776a555eaedf2f331afcc55c2c9
MegaSuperPuperDeveloper <133245874+MegaSuperPuperDeveloper@users.noreply.github.com>
2023-09-30 11:52:14 +0300

    Update Program.cs

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -1,4 +1,9 @@
-﻿int CountOfNumber(int num) {
+// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+int CountOfNumber(int num) {
     if(num == 0) return 1;
     int count = 0;
     while(num != 0) {
