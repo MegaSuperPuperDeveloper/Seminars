ef81b1d7d84ce5c8a79948f33701fd87e7702117
Your Name <you@example.com>
2023-09-30 11:48:41 +0300

    V1

---
+++ Task26/Program.cs
@@ -0,0 +1,13 @@
+﻿int CountOfNumber(int num) {
+    if(num == 0) return 1;
+    int count = 0;
+    while(num != 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{A} -> {CountOfNumber(A)}");
