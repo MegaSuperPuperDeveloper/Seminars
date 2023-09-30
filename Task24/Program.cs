6223a0477f873688057bb9775bffe4d1001408c1
Your Name <you@example.com>
2023-09-30 11:48:29 +0300

    V1

---
+++ Task24/Program.cs
@@ -0,0 +1,13 @@
+﻿int Summa(int num) {
+    int sum = 0;
+    for(int i = 1; i <= num; i++) {
+        checked {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{A} -> {Summa(A)}");
