50eabb3b44b60733205f99c6a54eabf8493db69c
Your Name <you@example.com>
2023-09-30 11:48:48 +0300

    V1

---
+++ Task28/Program.cs
@@ -0,0 +1,13 @@
+﻿int factorial(int num) {
+    int sum = 1;
+    for(int i = 1; i <= num; i++) {
+        checked {
+            sum *= i;
+        }
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{A} -> {factorial(A)}");
